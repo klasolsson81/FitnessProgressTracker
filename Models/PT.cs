379bfd9040d0b8a24e399e111970791950e62d21
Sacad elmi <sacadzidan@gmail.com>
2025-11-05 11:46:06 +0100

    Strukturerart upp Models-mappen och även skapat olika klasser som ska ligga här på models mappar.

---
+++ Models/PT.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgressTracker.Models
+{
+	// skapat pt klass som ärver från user klassen
+	internal class PT: User
+	{
+
+	}
+}
---
+++ Models/WorkoutPlan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgressTracker.Models
+{
+	// skapat workoutplan klass som kommer att användas för att skapa träningsplaner för klienter
+	internal class WorkoutPlan
+	{
+
+	}
+}
