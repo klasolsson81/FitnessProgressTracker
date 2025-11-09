18c493b9d1c254d5d315fcab8c38409cbef39ab6
Sacad elmi <sacadzidan@gmail.com>
2025-11-09 14:03:37 +0100

    Implementerat SpectreUIHelper med Spectre.Console"

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
-﻿namespace FitnessProgressTracker
+﻿using FitnessProgressTracker.UI;
+
+namespace FitnessProgressTracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Vår GitHub Action fungerar!");
+			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Vår GitHub Action fungerar!");
 
-            Console.WriteLine("\nTryck valfri tangent för att avsluta...");
-            Console.ReadKey();
-        }
+			Console.WriteLine("\nTryck valfri tangent för att avsluta...");
+			Console.ReadKey();
+
+
+
+
+
+
+
+
+		}
     }
 }
--- UI/Menu.cs
+++ UI/Menu.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Spectre.Console;
 
 namespace FitnessProgressTracker.UI
 {
     public class Menu
-    {
-    }
+	{
+
+     
+	}
+
 }
+
